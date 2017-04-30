9351afd1ca85353fdac49a8bbc07ec289c40fc2d
Eugene Kovaljov <didevgen@gmail.com>
2017-04-30 13:09:34 +0300

    Add connection service

---
+++ iot/ZKAccess/src/ZKAccess/State/IConnection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZKAccess.Entity;
+
+namespace ZKAccess.State
+{
+    interface IConnection
+    {
+        void Connect(string deviceId, ConnectionParams connection);
+
+        bool Disconnect(string deviceId);
+    }
+}
