fac96aef0395807824b21826294cb3f307898d51
Eugene Kovaljov <didevgen@gmail.com>
2017-05-06 20:16:07 +0300

    Delete test controller

---
+++ backend/src/main/java/ua/nure/providence/controllers/history/HistoryController.java
@@ -0,0 +1,14 @@
+package ua.nure.providence.controllers.history;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Providence Team on 06.05.2017.
+ */
+@RestController
+@Transactional
+@RequestMapping("history")
+public class HistoryController {
+}
