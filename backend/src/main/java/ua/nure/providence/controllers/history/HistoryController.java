d5c7e9818a3d8dbf5047d78919ac13df95530d94
Eugene Kovaljov <didevgen@gmail.com>
2017-05-06 22:33:31 +0300

    Remove stub script

--- backend/src/main/java/ua/nure/providence/controllers/history/HistoryController.java
+++ backend/src/main/java/ua/nure/providence/controllers/history/HistoryController.java
@@ -34,13 +34,7 @@ public class HistoryController {
     @Autowired
     private HistoryDAO dao;
 
-    @Autowired
-    private CardHolderDAO holderDAO;
-
-    @Autowired
-    private RoomDAO roomDAO;
-
-    /*@RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
     public ResponseEntity<List<HistoryDTO>> getAccountHistory(@RequestParam(value = "limit", required = false, defaultValue = "0") long limit,
                                                               @RequestParam(value = "offset", required = false, defaultValue = "0") long offset) {
@@ -52,36 +46,6 @@ public class HistoryController {
         List<History> histories = dao.getAccountHistory(token.getAuthenticatedUser(), limit, offset);
         return ResponseEntity.ok(histories.stream().map(history -> new HistoryDTO().convert(history))
                 .collect(Collectors.toList()));
-    }*/
-
-    @RequestMapping(value = "", method = RequestMethod.GET)
-    @ResponseBody
-    public ResponseEntity getAccountHistory(@RequestParam(value = "limit", required = false, defaultValue = "0") long limit,
-                                            @RequestParam(value = "offset", required = false, defaultValue = "0") long offset) {
-        if (limit == 0) {
-            limit = Long.MAX_VALUE;
-        }
-        LoginToken token = (LoginToken) SecurityContextHolder.getContext().getAuthentication();
-        List<CardHolder> holders = holderDAO.getAll(token.getAuthenticatedUser().getAccount(), 7, offset);
-        List<Room> rooms = roomDAO.getAll(token.getAuthenticatedUser().getAccount(), 7, offset);
-        List<EventType> types = dao.getEventTypes();
-        holders.forEach(holder -> {
-            rooms.forEach(room -> {
-                for (int i = 0; i < 40; i++) {
-                    History history = new History();
-                    history.setCardHolder(holder);
-                    history.setRoom(room);
-                    history.setInOutState(new Random().nextInt(2));
-                    history.setEventType(types.stream().filter(type -> type.getId() == 1).findFirst().get());
-                    DateTime time = new DateTime();
-                    time = time.minusDays(new Random().nextInt(30));
-                    time = time.minusHours(new Random().nextInt(24));
-                    time = time.minusMinutes(new Random().nextInt(30));
-                    history.setTimeStamp(time);
-                    dao.insert(history);
-                }
-            });
-        });
-        return new ResponseEntity(HttpStatus.OK);
     }
+
 }
